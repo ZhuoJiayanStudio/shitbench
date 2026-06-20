32db4420ebd118aec6d425704bf1b3600cff9f02
卓嘉彦工作室 <xiaozhuo.os2024@outlook.com>
2026-06-20 19:56:56 +0800

    Add Disclaimer of Warranty and Limitation of Liability

--- main.py
+++ main.py
@@ -39,6 +39,7 @@ def RAM() -> int:
         a.pop(0)
     print(f"The RAM's score is {int(10000 // (time.time() - theTime))}/10000")
     return 0
+
 def VRM() -> int:
     theTime: int = time.time()
     sys.set_int_max_str_digits(1000000)
@@ -46,24 +47,41 @@ def VRM() -> int:
         99999 ** 99999
         time.sleep(0.05)
     print(f"The VRM's score is {int(10000 // (time.time() - theTime))}/10000")
+    return 0
 
 def main() -> int:
     print("Welcome to shitbench")
     print("Copyright (C)2026 Zhuo Jiayan")
     print("This program licensed under Mozilla Public License version 2.0")
-    print("We'll test:")
-    print("1. CPU")
-    print("2. RAM")
-    print("3. VRM")
-    p = psutil.Process()
-    p.cpu_affinity([0])
-    if input("OK?(Press \"OK\")") == "OK":
-        print("Running the shit code......")
-        CPU()
-        RAM()
-        VRM()
-        input("Press enter to quit")
+    print("Please choose a mode:")
+    print("1. bench")
+    try:
+        mode = int(input())
+    except ValueError:
         quit()
+        
+    if mode == 1:
+        print("We'll test:")
+        print("1. CPU")
+        print("2. RAM")
+        print("3. VRM")
+        print("Disclaimer of Warranty(From MPL)")
+        print("Covered Software is provided under this License on an \"as is\" basis, without warranty of any kind, either expressed, implied, or statutory, including, without limitation, warranties that the Covered Software is free of defects, merchantable, fit for a particular purpose or non-infringing. The entire risk as to the quality and performance of the Covered Software is with You. Should any Covered Software prove defective in any respect, You (not any Contributor) assume the cost of any necessary servicing, repair, or correction. This disclaimer of warranty constitutes an essential part of this License. No use of any Covered Software is authorized under this License except under this disclaimer.")
+        print("Limitation of Liability(From MPL)")
+        print("Under no circumstances and under no legal theory, whether tort (including negligence), contract, or otherwise, shall any Contributor, or anyone who distributes Covered Software as permitted above, be liable to You for any direct, indirect, special, incidental, or consequential damages of any character including, without limitation, damages for lost profits, loss of goodwill, work stoppage, computer failure or malfunction, or any and all other commercial damages or losses, even if such party shall have been informed of the possibility of such damages. This limitation of liability shall not apply to liability for death or personal injury resulting from such party's negligence to the extent applicable law prohibits such limitation. Some jurisdictions do not allow the exclusion or limitation of incidental or consequential damages, so this exclusion and limitation may not apply to You.")
+        
+        p = psutil.Process()
+        p.cpu_affinity([0])
+        
+        if input("Can you accept the Disclaimer of Warranty and Limitation of Liability?(y/n)") == "y":
+            print("Running the shit code......")
+            CPU()
+            RAM()
+            VRM()
+            input("Press enter to quit")
+            quit()
+        else:
+            quit()
     else:
         quit()
 
